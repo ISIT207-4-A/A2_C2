cf8e1823ba8ec32b4e3d208ef95166b6a3fac860
ngvh2605 <48985636+ngvh2605@users.noreply.github.com>
2020-09-24 10:28:26 +1000

    Add files via upload

--- 2-2.js
+++ 2-2.js
@@ -17,11 +17,13 @@ function openTab(evt, tabName) {
 
     $(document).ready(function(){
           $("#hide").click(function(){
-            $("p.intro").hide();
+            $("p.intro").hide("fast", function(){ alert("The paragraph is hidden"); }
+            );
           });
 
           $("#show").click(function(){
-            $("p.intro").show();
+            $("p.intro").show("fast", function(){ alert("The paragraph is shown"); }
+            );
           });
 
           //fill table
@@ -59,8 +61,27 @@ function openTab(evt, tabName) {
           $("#changeImage").click(function(){
             let link = $("#imageLink").val();
             $("#image1").attr('src', link);
+            var txt="";
+            txt+= "Width of image: "+$("#image1").width() + "</br>";
+            txt+= "Height of image: "+$("#image1").height() + "<br /><br />";
+            $("#sizeImg").html(txt);
           });
         });
+//offset
+$(document).ready(function(){
+  $("#offsetImg").click(function(){
+    var x = $("#image1").offset();
+    alert("x = " + x.top + ", y = " + x.left);
+  });
+});
+
+//position
+$(document).ready(function(){
+  $("#positionImg").click(function(){
+    var x = $("#image1").position();
+    alert("Top position: " + x.top + ", Left position: " + x.left);
+  });
+});
 
 //animate()
 $(document).ready(function(){
@@ -108,5 +129,30 @@ $(document).ready(function(){
 $(document).ready(function(){
   $("#rolling").click(function(){
     $("#curtain").slideToggle("slow");
+  });
+});
+
+//queue
+$(document).ready(function(){
+    var boxQ = $("#boxQueue");
+    setInterval(function(){ $("#numQueue").text(boxQ.queue().length);  }, 1);
+  $("#startQueue").click(function(){
+ 
+    boxQ.animate({opacity: 100, width: '500px', right: '250px'}, 05000);
+    boxQ.css("background-color", "#1E90FF");
+    boxQ.animate({opacity: 100, width: '100px', left: '250px'}, 3000);
+    
+  });
+  $("#stopQueue").click(function(){
+    boxQ.clearQueue();
+    $("#numQueue").text(boxQ.queue().length);  
+  });
+});
+
+
+//addClass
+$(document).ready(function(){
+  $("#changeCSS").click(function(){
+    $("#changeCSS").addClass("button");
   });
 });
